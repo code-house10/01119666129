bf01205b6d38438e34bf4b2f2011b931f50911ce
code-house10 <107010368+code-house10@users.noreply.github.com>
2025-01-07 19:22:24 +0200

    commit

---
+++ js/site-public.js
@@ -0,0 +1,88 @@
+﻿(function ($) {
+    "use strict";
+
+    //$('.theme-loader').addClass('loaded');
+    $('.theme-loader').animate({
+        'opacity': '0',
+    }, 1200);
+
+    setTimeout(function () {
+        $('.theme-loader').remove();
+    }, 2000);
+
+    // Initiate the wowjs
+    new WOW().init();
+
+   
+
+    $('.counter').counterUp({
+        time: 2000
+    });
+
+    // testimonial carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        center: false,
+        dots: false,
+        rtl: true,
+        loop: true,
+        margin: 25,
+        nav: true,
+        navText: [
+            '<i class="fa fa-arrow-right"></i>',
+            '<i class="fa fa-arrow-left"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 2
+            },
+            1200: {
+                items: 2
+            }
+        }
+    });
+
+    $(document).on('ready', function () {
+        $('select').niceSelect();
+    });
+    // menu fixed js code
+    $(window).on('scroll', function () {
+        var window_top = $(window).scrollTop() + 1;
+        if (window_top > 50) {
+            $('.main_menu').addClass('menu_fixed animated fadeInDown');
+        } else {
+            $('.main_menu').removeClass('menu_fixed animated fadeInDown');
+        }
+    });
+
+    const progressCircle = document.querySelector(".autoplay-progress svg");
+    const progressContent = document.querySelector(".autoplay-progress span");
+
+    var HomeSwiper = new Swiper(".banner_video", {
+        effect: "fade",
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false
+        },
+        on: {
+            autoplayTimeLeft(s, time, progress) {
+                progressCircle.style.setProperty("--progress", 1 - progress);
+                progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+            }
+        }
+    });
+
+}(jQuery));
